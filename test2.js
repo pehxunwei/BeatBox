852793a2aa9e0d1ebd4a2a681ee58c2177681091
pehxunwei <45027722+pehxunwei@users.noreply.github.com>
2018-11-14 14:34:48 +0800

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,28 @@
+    <View>
+    <View style={{height: 40, width: 100 + '%', backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={{fontWeight: 'bold'}}>
+        <Text style={{color: 'red'}}>
+        Please place thumb on sensor
+        </Text>
+    </Text>    
+    </View>
+    <View style={{height: 420, width: 100 + '%', backgroundColor: '#fff', alignItems: 'center', paddingTop: 350}}>
+    <Text style={{fontWeight: 'bold', fontSize: 20}}>PPG Curve</Text>
+    <View style={{ flexDirection: 'row'}}>
+    <View style={{height: 300, width: 210, backgroundColor: '#4f3d5c', paddingRight: 0, paddingTop: 50, alignItems: 'center'}}>
+    <View style={{borderRadius: 150, height: 150, width: 150, backgroundColor: 'red'}}>
+    <Text style={{fontWeight: 'bold', bottom: -18, left: 17, color: 'black', fontSize: 90}}>63</Text>
+    <Text style={{fontWeight: 'bold', bottom: -60, left: 20, fontSize: 20}}>Heart Rate</Text>
+    
+    </View>
+    </View>
+    <View style={{height: 300, width: 210, backgroundColor: '#4d5c9c', alignItems: 'center', paddingTop: 50}}>
+    <View style={{borderRadius: 150, height: 150, width: 150, backgroundColor: 'green'}}>
+    <Text style={{fontWeight: 'bold', bottom: -18, left: 17, color: 'black', fontSize: 90}}>98</Text>
+    <Text style={{fontWeight: 'bold', bottom: -60, left: 50, fontSize: 20}}>SpO2</Text>
+    
+    </View>
+    </View>
+    </View>
+    </View>
+    </View>
